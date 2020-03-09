fb3a6a05ad836fbdb6daa98ebb1cbdda4cf3a29d
murisu3955 <59944031+murisu3955@users.noreply.github.com>
2020-03-09 15:42:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "purple";
+ctx.lineWidth = 2.5;
+
 
 let painting = false;
+
+function stopPainting(){
+    painting = false;
+}
+
+function startPainting(){
+    painting = true;
+}
 
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
     console.log(x,y);
 }
 
@@ -12,13 +35,10 @@ function onMouseDown(event){
     painting = true;
 }
 
-function onMouseUp(event){
-    painting = false;
-}
-
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting)
 }
