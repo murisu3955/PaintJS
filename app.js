7188171e7c4f85298845f911a7d66cf79ee9afda
murisu3955 <59944031+murisu3955@users.noreply.github.com>
2020-03-10 15:32:23 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -28,13 +29,12 @@ function onMouseMove(event){
         ctx.lineTo(x, y);
         ctx.stroke();
     }
-    console.log(x,y);
 }
 
-function onMouseDown(event){
-    painting = true;
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
-
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
@@ -42,3 +42,6 @@ if(canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting)
 }
+
+console.log(Array.from(colors));
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
